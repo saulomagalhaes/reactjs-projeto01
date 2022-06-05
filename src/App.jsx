d4003e0687c6b77283333025140fa947c31fff73
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-06-05 19:12:33 -0300

    feat(post component): implementacao do componente post + estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './components/Post';
 
 import styles from './App.module.css';
 
-import { Sidebar } from './components/Sidebar';
 import './global.css';
 
 function App() {
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,43 @@
-export function Post(props) {
+import styles from './Post.module.css';
+
+export function Post() {
   return (
-    <>
-      <p>{props.author}</p>
-      <p>{props.content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/94480172?v=4"
+          />
+          <div>
+            <strong>Saulo Magalhães</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13" dateTime="2022-">
+          Publicado há 1 hora
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>
+          <p>Fala galeraa 👋</p>
+          <p>
+            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
+            🚀
+          </p>
+          <p>
+            <a href="#">jane.design/doctorcare</a>
+          </p>
+          <p>
+            <a href="#">#novoprojeto</a>{' '}
+            <a href="#">#nlw</a>{' '}
+            <a href="#">#rocketseat</a>
+          </p>
+        </p>
+      </div>
+    </article>
   );
 }
