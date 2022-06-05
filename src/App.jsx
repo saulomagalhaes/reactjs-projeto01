51c789c4a92f202aee173ce44012f54964e491df
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-06-05 18:31:25 -0300

    feat(navbar): implementacao do navbar + css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import { Header } from './components/Header';
+import { Post } from './components/Post';
 
+import styles from './App.module.css';
+
+import { Sidebar } from './components/Sidebar';
 import './global.css';
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello World</h1>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Saulo Alves Magalhães"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae cum dolorem sunt  quos quidem nemo odit cupiditate ipsa odio temporibus"
+          />
+          <Post
+            author="Carolina Ribeiro Barbosa"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae cum dolorem sunt  quos quidem nemo odit cupiditate ipsa odio temporibus"
+          />
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props) {
+  return (
+    <>
+      <p>{props.author}</p>
+      <p>{props.content}</p>
+    </>
+  );
+}
