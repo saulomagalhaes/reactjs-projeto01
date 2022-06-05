57067ad9f4b07d85b04690337bd222edb48ecf97
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-06-05 14:20:26 -0300

    feat(header): implementando o header da aplicacao, alem de alguns estilos css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header';
+
+import './global.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
